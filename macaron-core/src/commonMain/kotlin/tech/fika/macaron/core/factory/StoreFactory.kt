704b10f2e79a0911ff217696b6e65e2c77b9c4ad
wasabi-muffin <gmvalentino8@gmail.com>
2023-09-10 03:26:40 +0900

    fix: remove result and events from store factory

--- macaron-core/src/commonMain/kotlin/tech/fika/macaron/core/factory/StoreFactory.kt
+++ macaron-core/src/commonMain/kotlin/tech/fika/macaron/core/factory/StoreFactory.kt
@@ -2,53 +2,45 @@ package tech.fika.macaron.core.factory
 
 import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.Dispatchers
-import tech.fika.macaron.core.components.Interpreter
 import tech.fika.macaron.core.components.Middleware
 import tech.fika.macaron.core.components.Processor
 import tech.fika.macaron.core.components.Reducer
 import tech.fika.macaron.core.components.Store
 import tech.fika.macaron.core.contract.Action
-import tech.fika.macaron.core.contract.Event
 import tech.fika.macaron.core.contract.Intent
-import tech.fika.macaron.core.contract.Result
 import tech.fika.macaron.core.contract.State
 
 interface StoreFactory {
-    fun <I : Intent, A : Action, R : Result, S : State, E : Event> create(
+    fun <I : Intent, A : Action, S : State> create(
         initialState: S,
-        interpreter: Interpreter<I, A, S>,
-        processor: Processor<A, R, S, E>,
-        reducer: Reducer<R, S>,
-        middlewares: List<Middleware<I, A, R, S, E>> = emptyList(),
-        coroutineContext: CoroutineContext = Dispatchers.Main,
-    ): Store<I, S, E>
+        processor: Processor<I, A, S>,
+        reducer: Reducer<A, S>,
+        middlewares: List<Middleware<I, A, S>> = emptyList(),
+        coroutineContext: CoroutineContext = Dispatchers.Main.immediate,
+    ): Store<I, A, S>
 
-    fun <I : Intent, A : Action, R : Result, S : State, E : Event> create(
+    fun <I : Intent, A : Action, S : State> create(
         initialState: S,
-        interpreter: Interpreter<I, A, S>,
-        processor: Processor<A, R, S, E>,
-        reducer: Reducer<R, S>,
-        middlewares: List<Middleware<I, A, R, S, E>> = emptyList(),
-    ): Store<I, S, E> = create(
+        processor: Processor<I, A, S>,
+        reducer: Reducer<A, S>,
+        middlewares: List<Middleware<I, A, S>> = emptyList(),
+    ): Store<I, A, S> = create(
         initialState = initialState,
-        interpreter = interpreter,
         processor = processor,
         reducer = reducer,
         middlewares = middlewares,
-        coroutineContext = Dispatchers.Main
+        coroutineContext = Dispatchers.Main.immediate
     )
 
-    fun <I : Intent, A : Action, R : Result, S : State, E : Event> create(
+    fun <I : Intent, A : Action, S : State> create(
         initialState: S,
-        interpreter: Interpreter<I, A, S>,
-        processor: Processor<A, R, S, E>,
-        reducer: Reducer<R, S>,
-    ): Store<I, S, E> = create(
+        processor: Processor<I, A, S>,
+        reducer: Reducer<A, S>,
+    ): Store<I, A, S> = create(
         initialState = initialState,
-        interpreter = interpreter,
         processor = processor,
         reducer = reducer,
         middlewares = emptyList(),
-        coroutineContext = Dispatchers.Main
+        coroutineContext = Dispatchers.Main.immediate
     )
 }
