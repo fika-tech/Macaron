c2c3fae23c800b1e5267adfbf94f64cf46097bca
wasabi-muffin <gmvalentino8@gmail.com>
2023-09-10 03:27:23 +0900

    fix: remove result and events from statemachine

--- macaron-statemachine/src/commonMain/kotlin/tech/fika/macaron/statemachine/components/StateMachine.kt
+++ macaron-statemachine/src/commonMain/kotlin/tech/fika/macaron/statemachine/components/StateMachine.kt
@@ -1,118 +1,130 @@
 package tech.fika.macaron.statemachine.components
 
+import kotlin.coroutines.CoroutineContext
+import kotlin.reflect.KClass
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.flow
-import tech.fika.macaron.core.components.Interpreter
 import tech.fika.macaron.core.components.Middleware
 import tech.fika.macaron.core.components.Processor
 import tech.fika.macaron.core.components.Reducer
 import tech.fika.macaron.core.contract.Action
 import tech.fika.macaron.core.contract.Contract
-import tech.fika.macaron.core.contract.Event
 import tech.fika.macaron.core.contract.Intent
-import tech.fika.macaron.core.contract.Result
 import tech.fika.macaron.core.contract.State
 import tech.fika.macaron.core.factory.StoreFactory
-import kotlin.coroutines.CoroutineContext
-import kotlin.reflect.KClass
 
-fun <I : Intent, A : Action, R : Result, S : State, E : Event> StoreFactory.create(
+fun <I : Intent, A : Action, S : State> StoreFactory.create(
     initialState: S,
-    stateMachine: StateMachine<I, A, R, S, E>,
-    middlewares: List<Middleware<I, A, R, S, E>> = emptyList(),
+    stateMachine: StateMachine<I, A, S>,
+    middlewares: List<Middleware<I, A, S>> = emptyList(),
     coroutineContext: CoroutineContext = Dispatchers.Main,
 ) = create(
     initialState = initialState,
-    interpreter = stateMachine.interpreter,
     processor = stateMachine.processor,
     reducer = stateMachine.reducer,
     middlewares = middlewares,
-    coroutineContext = coroutineContext
+    coroutineContext = coroutineContext,
 )
 
-open class StateMachine<I : Intent, A : Action, R : Result, S : State, E : Event>(
-    val graph: Map<Matcher<S, S>, StateNode<I, A, R, S, E>>,
+@Suppress("UNCHECKED_CAST")
+open class StateMachine<I : Intent, A : Action, S : State>(
+    val stateMap: Map<Matcher<S, S>, StateNode<I, A, S>>,
 ) {
-    constructor(builder: StateMachineBuilder<I, A, R, S, E>.() -> Unit) :
-        this(StateMachineBuilder<I, A, R, S, E>().apply(builder).build().graph)
+    constructor(builder: Builder<I, A, S>.() -> Unit) : this(Builder<I, A, S>().apply(builder).build().stateMap)
 
-    val interpreter: Interpreter<I, A, S> get() = StateMachineInterpreter(this)
-    val processor: Processor<A, R, S, E> get() = StateMachineProcessor(this)
-    val reducer: Reducer<R, S> get() = StateMachineReducer(this)
+    val processor: Processor<I, A, S> get() = StateMachineProcessor(this)
+    val reducer: Reducer<A, S> get() = StateMachineReducer(this)
 
     class Matcher<T : Contract, out R : T> private constructor(private val kClass: KClass<R>) {
-        private val predicates = mutableListOf<(T) -> Boolean>({ kClass.isInstance(it) })
 
+        private val predicates = mutableListOf<(T) -> Boolean>({ kClass.isInstance(it) })
         fun matches(value: T) = predicates.all { it(value) }
 
         companion object {
             fun <T : Contract, R : T> any(kClass: KClass<R>): Matcher<T, R> = Matcher(kClass)
-
             inline fun <T : Contract, reified R : T> any(): Matcher<T, R> = any(R::class)
         }
     }
 
-    data class InterpreterNode<S : State, I : Intent>(val state: S, val intent: I)
-    data class ProcessorNode<S : State, A : Action>(val state: S, val action: A)
-    data class ReducerNode<S : State, R : Result>(val state: S, val result: R)
+    /* Nodes */
 
-    class StateNode<I : Intent, A : Action, R : Result, S : State, E : Event> {
-        val intents = mutableMapOf<Matcher<I, I>, InterpreterNode<S, I>.() -> A?>()
-        val actions = mutableMapOf<Matcher<A, A>, suspend (ProcessorNode<S, A>) -> Flow<SideEffect<R, E>>>()
-        val results = mutableMapOf<Matcher<R, R>, ReducerNode<S, R>.() -> S?>()
+    data class IntentNode<I : Intent, S : State>(val intent: I, val state: S)
+
+    data class ActionNode<A : Action, S : State>(val action: A, val state: S)
+
+    data class StateNode<I : Intent, A : Action, S : State>(
+        val intentMap: Map<Matcher<I, I>, suspend (IntentNode<I, S>) -> Flow<A>>,
+        val actionMap: Map<Matcher<A, A>, (ActionNode<A, S>) -> S?>,
+    )
+
+    /* Scopes */
+
+    data class IntentScope<I : Intent, A : Action, S : State>(
+        val intent: I,
+        val state: S,
+        internal val collector: FlowCollector<A>,
+    ) {
+        suspend fun emit(action: A) = collector.emit(action)
+        suspend operator fun A.unaryPlus() = emit(this)
     }
 
-    sealed class SideEffect<R : Result, E : Event> {
-        data class ResultNode<R : Result, E : Event>(val value: R) : SideEffect<R, E>()
-        data class EventNode<R : Result, E : Event>(val value: E) : SideEffect<R, E>()
-    }
+    data class ActionScope<A : Action, S : State>(val action: A, val state: S)
 
-    class StateMachineBuilder<I : Intent, A : Action, R : Result, S : State, E : Event> {
-        internal val graph = LinkedHashMap<Matcher<S, S>, StateNode<I, A, R, S, E>>()
+    /* Annotations */
 
-        fun <STATE : S> state(stateMatcher: Matcher<S, STATE>, config: StateNodeBuilder<STATE>.() -> Unit) {
-            graph[stateMatcher] = StateNodeBuilder<STATE>().apply(config).build()
+    @DslMarker
+    @Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
+    private annotation class StateMachineDsl
+
+    @DslMarker
+    @Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
+    private annotation class StateDsl
+
+    @StateMachineDsl
+    class Builder<I : Intent, A : Action, S : State> {
+        private val stateMap = LinkedHashMap<Matcher<S, S>, StateNode<I, A, S>>()
+
+        fun <STATE : S> state(stateMatcher: Matcher<S, STATE>, builder: @StateMachineDsl StateBuilder<STATE>.() -> Unit) {
+            stateMap[stateMatcher] = StateBuilder<STATE>().apply(builder).build()
         }
 
-        inline fun <reified STATE : S> state(noinline config: StateNodeBuilder<STATE>.() -> Unit) = state(Matcher.any(), config)
+        inline fun <reified STATE : S> state(noinline builder: @StateMachineDsl StateBuilder<STATE>.() -> Unit) = state(Matcher.any(), builder)
 
-        fun build(): StateMachine<I, A, R, S, E> = StateMachine(graph.toMap())
+        fun build(): StateMachine<I, A, S> = StateMachine(stateMap.toMap())
 
-        @Suppress("UNCHECKED_CAST")
-        inner class StateNodeBuilder<STATE : S> {
-            private val stateNode = StateNode<I, A, R, S, E>()
+        @StateDsl
+        inner class StateBuilder<STATE : S> {
+            private val intentMap = mutableMapOf<Matcher<I, I>, suspend (IntentNode<I, S>) -> Flow<A>>()
+            private val actionMap = mutableMapOf<Matcher<A, A>, (ActionNode<A, S>) -> S?>()
 
-            fun <INTENT : I> interpret(intentMatcher: Matcher<I, INTENT>, interpret: InterpreterNode<STATE, INTENT>.() -> A?) {
-                stateNode.intents[intentMatcher] = { interpret(this as InterpreterNode<STATE, INTENT>) }
-            }
-
-            inline fun <reified INTENT : I> interpret(noinline interpret: InterpreterNode<STATE, INTENT>.() -> A?) = interpret(Matcher.any(), interpret)
-
-            fun <ACTION : A> process(
-                actionMatcher: Matcher<A, ACTION>,
-                process: suspend FlowCollector<SideEffect<R, E>>.(ProcessorNode<STATE, ACTION>) -> Unit,
+            fun <INTENT : I> process(
+                intentMatcher: Matcher<I, INTENT>,
+                intent: @StateDsl suspend IntentScope<INTENT, A, STATE>.() -> Unit,
             ) {
-                stateNode.actions[actionMatcher] = { node ->
-                    flow { process(this, node as ProcessorNode<STATE, ACTION>) }
+                intentMap[intentMatcher] = { (intent, state) ->
+                    flow {
+                        intent(IntentScope(intent = intent, state = state, collector = this) as IntentScope<INTENT, A, STATE>)
+                    }
                 }
             }
 
-            inline fun <reified ACTION : A> process(noinline process: suspend FlowCollector<SideEffect<R, E>>.(ProcessorNode<STATE, ACTION>) -> Unit) =
-                process(Matcher.any(), process)
+            inline fun <reified INTENT : I> process(noinline intent: @StateDsl suspend IntentScope<INTENT, A, STATE>.() -> Unit) =
+                process(Matcher.any(), intent)
 
-            fun <RESULT : R> reduce(resultMatcher: Matcher<R, RESULT>, reduce: ReducerNode<STATE, RESULT>.() -> S?) {
-                stateNode.results[resultMatcher] = { reduce(this as ReducerNode<STATE, RESULT>) }
+            fun <ACTION : A> reduce(
+                actionMatcher: Matcher<A, ACTION>,
+                reduce: @StateDsl ActionScope<ACTION, STATE>.() -> S,
+            ) {
+                actionMap[actionMatcher] = { (action, state) ->
+                    reduce(ActionScope(action, state) as ActionScope<ACTION, STATE>)
+                }
             }
 
-            inline fun <reified RESULT : R> reduce(noinline reduce: ReducerNode<STATE, RESULT>.() -> S?) = reduce(Matcher.any(), reduce)
+            inline fun <reified ACTION : A> reduce(noinline action: @StateDsl ActionScope<ACTION, STATE>.() -> S) = reduce(Matcher.any(), action)
 
-            fun build() = stateNode
+            fun build(): StateNode<I, A, S> = StateNode(intentMap = intentMap, actionMap = actionMap)
         }
     }
 }
-
-suspend fun <R : Result, E : Event> FlowCollector<StateMachine.SideEffect<R, E>>.result(result: R) = emit(StateMachine.SideEffect.ResultNode(result))
-
-suspend fun <R : Result, E : Event> FlowCollector<StateMachine.SideEffect<R, E>>.event(event: E) = emit(StateMachine.SideEffect.EventNode(event))
--- macaron-statemachine/src/commonMain/kotlin/tech/fika/macaron/statemachine/components/StateMachineReducer.kt
+++ macaron-statemachine/src/commonMain/kotlin/tech/fika/macaron/statemachine/components/StateMachineReducer.kt
@@ -2,18 +2,17 @@ package tech.fika.macaron.statemachine.components
 
 import tech.fika.macaron.core.components.Reducer
 import tech.fika.macaron.core.contract.Action
-import tech.fika.macaron.core.contract.Event
 import tech.fika.macaron.core.contract.Intent
-import tech.fika.macaron.core.contract.Result
 import tech.fika.macaron.core.contract.State
 
-class StateMachineReducer<I : Intent, A : Action, R : Result, S : State, E : Event>(
-    private val stateMachine: StateMachine<I, A, R, S, E>,
-) : Reducer<R, S> {
-    override suspend fun reduce(result: R, state: S): S? = stateMachine.graph
+class StateMachineReducer<I : Intent, A : Action, S : State>(
+    private val stateMachine: StateMachine<I, A, S>,
+) : Reducer<A, S> {
+    override suspend fun reduce(action: A, state: S): S = stateMachine.stateMap
         .filterKeys { key -> key.matches(state) }.values
-        .flatMap { stateNode -> stateNode.results.entries }
-        .find { resultMatcher -> resultMatcher.key.matches(result) }
+        .flatMap { stateNode -> stateNode.actionMap.entries }
+        .find { actionMatcher -> actionMatcher.key.matches(action) }
         ?.value
-        ?.invoke(StateMachine.ReducerNode(state, result))
+        ?.invoke(StateMachine.ActionNode(action = action, state = state))
+        ?: state
 }
