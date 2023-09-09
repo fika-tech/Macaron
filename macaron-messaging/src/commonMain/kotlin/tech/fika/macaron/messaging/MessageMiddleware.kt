59b911d3180b220fe25fb31aee0089259a5e5629
wasabi-muffin <gmvalentino8@gmail.com>
2023-09-10 03:26:28 +0900

    fix: remove result and events from middleware classes

--- macaron-messaging/src/commonMain/kotlin/tech/fika/macaron/messaging/MessageMiddleware.kt
+++ macaron-messaging/src/commonMain/kotlin/tech/fika/macaron/messaging/MessageMiddleware.kt
@@ -7,29 +7,22 @@ import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
 import tech.fika.macaron.core.components.Middleware
 import tech.fika.macaron.core.contract.Action
-import tech.fika.macaron.core.contract.Event
 import tech.fika.macaron.core.contract.Intent
-import tech.fika.macaron.core.contract.Result
 import tech.fika.macaron.core.contract.State
 
-open class MessageSenderMiddleware<I : Intent, A : Action, R : Result, S : State, E : Event>(
+open class MessageSenderMiddleware<I : Intent, A : Action, S : State>(
     private val messageManager: MessageManager,
     private val sendAction: ((A) -> Message?)? = null,
-    private val sendResult: ((R) -> Message?)? = null,
-) : Middleware<I, A, R, S, E> {
+) : Middleware<I, A, S> {
     override fun modifyActions(actions: Flow<A>, state: () -> S): Flow<A> = actions.onEach { action ->
         sendAction?.invoke(action)?.let { messageManager.send(it) }
     }
-
-    override fun modifyResults(results: Flow<R>, state: () -> S): Flow<R> = results.onEach { result ->
-        sendResult?.invoke(result)?.let { messageManager.send(it) }
-    }
 }
 
-open class MessageReceiverMiddleware<I : Intent, A : Action, R : Result, S : State, E : Event>(
+open class MessageReceiverMiddleware<I : Intent, A : Action, S : State>(
     private val messageManager: MessageManager,
     private val receive: (Message) -> I?,
-) : Middleware<I, A, R, S, E> {
+) : Middleware<I, A, S> {
     override fun modifyIntents(intents: Flow<I>, state: () -> S): Flow<I> = merge(
         intents, messageManager.messages.map(receive).filterNotNull()
     )
--- macaron-statesaver/src/commonMain/kotlin/tech/fika/macaron/statesaver/StateSaverMiddleware.kt
+++ macaron-statesaver/src/commonMain/kotlin/tech/fika/macaron/statesaver/StateSaverMiddleware.kt
@@ -4,13 +4,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onEach
 import tech.fika.macaron.core.components.Middleware
 import tech.fika.macaron.core.contract.Action
-import tech.fika.macaron.core.contract.Event
 import tech.fika.macaron.core.contract.Intent
-import tech.fika.macaron.core.contract.Result
 import tech.fika.macaron.core.contract.State
 
-class StateSaverMiddleware<I : Intent, A : Action, R : Result, S : State, E : Event>(
+class StateSaverMiddleware<I : Intent, A : Action, S : State>(
     private val stateSaver: StateSaver<S>,
-) : Middleware<I, A, R, S, E> {
+) : Middleware<I, A, S> {
     override fun modifyStates(states: Flow<S>): Flow<S> = states.onEach(stateSaver::save)
 }
